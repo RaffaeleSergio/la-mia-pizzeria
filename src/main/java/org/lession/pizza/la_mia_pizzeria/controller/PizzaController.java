b50a8b5890c4d1301ae078d0f6c5bc95cdc2b933
RaffaeleSergio <raffaele16590@gmail.com>
2025-06-22 19:24:14 +0200

    implementazione

--- src/main/java/org/lession/pizza/la_mia_pizzeria/controller/PizzaController.java
+++ src/main/java/org/lession/pizza/la_mia_pizzeria/controller/PizzaController.java
@@ -1,10 +1,14 @@
 package org.lession.pizza.la_mia_pizzeria.controller;
 
+import java.util.Optional;
+
+import org.lession.pizza.la_mia_pizzeria.model.Pizza;
 import org.lession.pizza.la_mia_pizzeria.repository.PizzaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,8 +18,20 @@ public class PizzaController {
     private PizzaRepository pizzaRepository;
 
     @GetMapping
-    public String index(Model model){
+    public String index(Model model) {
         model.addAttribute("pizze", pizzaRepository.findAll());
         return "pizze/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") Integer id, Model model) {
+        Optional<Pizza> result = pizzaRepository.findById(id);
+
+        if (result.isPresent()) {
+            model.addAttribute("pizza", result.get());
+            return "pizze/show";
+        } else {
+            return "redirect:/pizze";
+        }
     }
 }
--- src/main/java/org/lession/pizza/la_mia_pizzeria/repository/PizzaRepository.java
+++ src/main/java/org/lession/pizza/la_mia_pizzeria/repository/PizzaRepository.java
@@ -1,8 +1,10 @@
 package org.lession.pizza.la_mia_pizzeria.repository;
 
+import java.util.List;
+
 import org.lession.pizza.la_mia_pizzeria.model.Pizza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PizzaRepository extends JpaRepository <Pizza, Integer> {
-    
+    List<Pizza> findByNomeContainingIgnoreCase(String nome);
 }
